daa008cd3904012f48480fe72de7f06762c3fcc7
Yanet Ennis <yanet.ennis@gmail.com>
2024-02-20 11:04:54 +0000

    Hidden wordcount

--- packages/koenig-lexical/demo/components/WordCount.jsx
+++ packages/koenig-lexical/demo/components/WordCount.jsx
@@ -1,11 +1,11 @@
 const WordCount = ({wordCount, tkCount}) => {
     return (
         <div className="absolute left-6 top-4 z-20 block cursor-pointer rounded bg-white px-2 py-1 font-mono text-sm tracking-tight text-grey-600 dark:bg-transparent">
-            <span data-testid="word-count">{wordCount}</span> words
+            <span data-testid="word-count" id="word-count">{wordCount}</span> words
             {tkCount > 0 && (
                 <>
                     {' '}
-                    / <span data-testid="tk-count" id="word-count">{tkCount}</span> TK{tkCount > 1 && 's'}
+                    / <span data-testid="tk-count">{tkCount}</span> TK{tkCount > 1 && 's'}
                 </>
             )}
         </div>
