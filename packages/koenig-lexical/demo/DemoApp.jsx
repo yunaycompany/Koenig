656619b15567677a3ebd310f68d206a055d9ada4
Yanet Ennis <yanet.ennis@gmail.com>
2024-04-23 09:25:46 +0100

    Add scroll again

--- packages/koenig-lexical/demo/DemoApp.jsx
+++ packages/koenig-lexical/demo/DemoApp.jsx
@@ -256,7 +256,7 @@ function DemoComposer({editorType, isMultiplayer, setWordCount, setTKCount}) {
             nodes={getAllowedNodes({editorType})}
         >
             <div className={`koenig-demo relative h-full grow ${darkMode ? 'dark' : ''}`} style={{'--kg-breakout-adjustment': isSidebarOpen ? '440px' : '0px'}}>
-                <div ref={containerRef} className="h-full  overflow-hidden">
+                <div ref={containerRef} className="h-full overflow-x-hidden">
                     <div className="mx-auto max-w-[740px] px-6 py-[15vmin] lg:px-0">
                         {showTitle
                             ? <TitleTextBox ref={titleRef} editorAPI={editorAPI} setTitle={updateTitle} title={title} />
@@ -267,7 +267,7 @@ function DemoComposer({editorType, isMultiplayer, setWordCount, setTKCount}) {
                             editorType={editorType}
                             registerAPI={setEditorAPI}
                             setTKCount={setTKCount}
-                            setWordCount={setWordCount}
+
                         />
                     </div>
                 </div>
